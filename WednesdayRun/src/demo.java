b9fda91a19b47c90e53968b61be88ed3c95ac659
bpolhamus13 <bpolhamus13@gmail.com>
2016-08-15 21:12:19 -0400

    Uploaded simple walkthrough final

---
+++ WednesdayRun/src/demo.java
@@ -0,0 +1,16 @@
+
+public class demo {
+	
+	static int week =5;
+	static int amount = 25;
+	static int salary =0;
+	public static void functions(){
+		salary = week*amount;
+		System.out.println(salary);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		functions();
+	}
+
+}
